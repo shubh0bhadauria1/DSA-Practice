f87b565a77d3b2155ab1b1bc9437b96400f3c213
Shubh Singh Bhadauriya <shubhsinghbhadauria74@gmail.com>
2026-03-07 20:17:09 +0530

    To check which Number is greater

--- Practice-1.cpp
+++ Practice-1.cpp
@@ -1,5 +1,40 @@
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"yes";
-}
+// //Take two integers input, a and b : a>b, and find the
+// //remainder when a is divided by b
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     // int x;
+//     // cout<<"enter\n";
+//     // cin>>x;
+//     // if(x%2==0)cout<<"even"; else cout<<"odd";
+//     // int x=16;
+//     // if(x%5==0 & x%3==0)cout<<"Yes";else cout<<"No";
+//     // int x,y,z;
+//     // cin>>x>>y>>z;
+//     // cout<<"max ";if(x>y && x>z)cout<<x;else if(y>z && y>x)cout<<y;else cout<<z;
+//     // float a,b;int x;
+//     // cout<<"Enter two numbers: ";
+//     // cin>>a>>b;cout<<"Enter\n1--> +\n2--> -\n3--> *\n4--> /\n";cin>>x;
+//     // switch (x)
+//     // {
+//     // case 1: cout<<(a+b);break;case 2: cout<<a-b;break; case 3: cout<<a*b;break; case 4: cout<<a/b; break;
+    
+//     // default:
+//     //     break;
+//     // }   
+//     //int n;cin>>n;
+//     // for(int i=1;i<=n;i++) cout<<i*19<<"\n";cout<<"Success";
+//     //for(int i=1;i<=2*n-1;i+=2)cout<<i<<" ";
+//     // int j=1 ;
+//     // while ( j <= 10 ) {
+//     // cout << j << endl;
+//     // j = j + 1 ;
+//     // }
+// //     int x;
+// // for (x = 0; x <= 255; x++) {
+// // cout << "Ascii value of Character " << (char)('a' + x) ;
+// // cout << " is " << x << endl;}
+//     for(int i=65; i<=90;i++)cout<<char(i)<<" "<<i<<"\n";
+
+// }
+//git add .; git commit -m "Added new cpp file"; git push
---
+++ Practice_Ques-1.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;cout<<"Enter two numbers\n";cin>>a>>b;
+    (a>b)?cout<<"first number "<<a<<" is greater":cout<<"second number "<<b<<" is greater";
+}
