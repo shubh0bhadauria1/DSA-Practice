b8499e39430a4cb0cb175619e4045d98845b5575
Shubh Singh Bhadauriya <shubhsinghbhadauria74@gmail.com>
2026-09-03 15:38:40 +0530

    Count the no. of elements greater than x in an array

---
+++ Count the number of elements strictly greater than x.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+int x;
+cout<<"Enter value of x: ";
+cin>>x;
+int a[5];
+cout<<"Enter 5 elements for the array: "<<endl;
+for(int i=0;i<5;i++) cin>>a[i];
+int ans=0;
+for(int i=0;i<5;i++){
+if(a[i]>x) ans++;
+}
+cout<<ans<<endl;
+
+}
