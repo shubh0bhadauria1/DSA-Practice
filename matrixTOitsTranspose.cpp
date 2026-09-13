10859652837bc536ec11b374ba78ef5f62a436b7
Shubh Singh Bhadauriya <shubhsinghbhadauria74@gmail.com>
2026-09-13 21:35:26 +0530

    Changing matrix to its transpose

---
+++ matrixTOitsTranspose.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a[3][3]={1,2,3,4,5,6,7,8,9};
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"\n\n";
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(i==j){
+                continue;
+            }
+            else if(i<j){
+                int temp;
+                temp=a[i][j];
+                a[i][j]=a[j][i];
+                a[j][i]=temp;
+
+            }
+        }
+
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
