5d318d24270df4b5bf1641da24d31ced3f6dea62
Shubh Singh Bhadauriya <shubhsinghbhadauria74@gmail.com>
2026-09-26 03:21:50 +0530

    Wave Print 2

---
+++ WavePrint2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int m;
+    int n;
+    cout<<"Enter Rows Of Array: ";
+    cin>>m;
+    cout<<"Enter Coloumns Of Array: ";
+    cin>>n;
+    int arr[m][n];
+    //Input Array Elements.
+    cout<<"Enter Elements Of Array: "<<endl;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    //Printing array in wave.
+    for(int i=m-1;i>=0;i--){
+
+        if(i%2==0){
+            for(int j=0;j<n;j++){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+        else{
+            for(int j=n-1;j>=0;j--){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+    }
+
+
+}
